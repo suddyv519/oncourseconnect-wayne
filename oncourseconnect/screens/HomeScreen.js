522b6c62143dd9f08343dcc4ad4e1e0239a38347
Sudharshan Venkatesh <venkatesh.sudharshan@gmail.com>
2018-12-20 01:49:52 -0500

    added state vars and get methods for grades and misc ids

--- oncourseconnect/screens/HomeScreen.js
+++ oncourseconnect/screens/HomeScreen.js
@@ -1,188 +1,259 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
+    Image,
+    Platform,
+    ScrollView,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+    Button,
+    TextInput
 } from 'react-native';
-import { WebBrowser } from 'expo';
+import {WebBrowser} from 'expo';
 
-import { MonoText } from '../components/StyledText';
+import {MonoText} from '../components/StyledText';
 
-export default class HomeScreen extends React.Component {
-  static navigationOptions = {
-    header: null,
-  };
+class HomeScreen extends Component {
+    static navigationOptions = {
+        header: null,
+    };
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
-              style={styles.welcomeImage}
-            />
-          </View>
+    state = {
+        username: '',
+        password: '',
+        schoolIds: [],
+        courseIds: [],
+        yearIds: [],
+        loggedIn: false
+    };
 
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
+    login = () => {
+        console.log("attempting login...");
 
-            <Text style={styles.getStartedText}>Get started by opening</Text>
+        let url = `https://www.oncourseconnect.com/sso/login?id=wayne&userType=S&username=${this.state.username}&password=${this.state.password}`;
 
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
+        fetch(url, {
+            method: 'POST',
+            credentials: 'include',
+        })
+        .then((res) => {
+            let response = res.text();
+            console.log(response);
+            //console.log(response.indexOf('incorrect') === -1 ? "login success" : "login failed"); // return true if login successful
+            this.setState({loggedIn: true});
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    };
+
+    logout = () => {
+        console.log("attempting to log out...");
+        let url = 'https://www.oncourseconnect.com/account/logout';
+        fetch(url, {
+            credentials: 'include'
+        })
+        .then((response) => {
+            console.log(response);
+            this.setState({loggedIn: false});
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    };
+
+    getInfo = () => {
+        console.log("getting school and year id info...");
+        let url = `https://www.oncourseconnect.com/api/classroom/student/get_student_school_years?studentId=${this.state.username}`;
+        fetch(url, {
+            credentials: 'include'
+        })
+        .then((response) => {
+            console.log(response.body);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    };
+
+    getGrades = (schoolId, yearId) => {
+        console.log("getting grades...");
+        let url = `https://www.oncourseconnect.com/api/classroom/student/report_cards?schoolId=${schoolId}&schoolYearId=${yearId}&studentId=${this.state.username}`;
+        fetch(url, {
+            credentials: 'include'
+        })
+        .then((response) => {
+                console.log(response.body);
+            })
+        .catch((error) => {
+            console.error(error);
+        });
+    };
+
+
+    render() {
+        return (
+            <View style={styles.container}>
+                {!this.state.loggedIn ?
+                    <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+                        <Text style={styles.labelText}>Username</Text>
+                        <TextInput
+                            placeholder="Enter username"
+                            onChangeText={(text) => this.setState({username: text})}
+                        />
+                        <Text style={styles.labelText}>Password</Text>
+                        <TextInput
+                            placeholder="Enter password"
+                            onChangeText={(text) => this.setState({password: text})}
+                        />
+                        <Button style={styles.button} onPress={this.login} title='Login'/>
+
+                        <Button style={{padding:'10'}} title={'Get Grades'} onPress={this.getGrades}/>
+                    </ScrollView>
+                :
+                    <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+                        <Button style={{padding:'10'}} title={'Get Grades'} onPress={this.getGrades}/>
+                        <Button style={styles.button} onPress={this.logout} title='Logout'/>
+                    </ScrollView>
+                }
+
+                <View style={styles.tabBarInfoContainer}>
+                    <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
+
+                    <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
+                        <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
+                    </View>
+                </View>
             </View>
-
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
-            </Text>
-          </View>
-
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
-          </View>
-        </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
-        </View>
-      </View>
-    );
-  }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
+        );
     }
-  }
 
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
+    _maybeRenderDevelopmentModeWarning() {
+        if (__DEV__) {
+            const learnMoreButton = (
+                <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
+                    Learn more
+                </Text>
+            );
 
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
+            return (
+                <Text style={styles.developmentModeText}>
+                    Development mode is enabled, your app will be slower but you can use useful development
+                    tools. {learnMoreButton}
+                </Text>
+            );
+        } else {
+            return (
+                <Text style={styles.developmentModeText}>
+                    You are not in development mode, your app will run at full speed.
+                </Text>
+            );
+        }
+    }
+
+    _handleLearnMorePress = () => {
+        WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
+    };
+
+    _handleHelpPress = () => {
+        WebBrowser.openBrowserAsync(
+            'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
+        );
+    };
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    developmentModeText: {
+        marginBottom: 20,
+        color: 'rgba(0,0,0,0.4)',
+        fontSize: 14,
+        lineHeight: 19,
+        textAlign: 'center',
+    },
+    contentContainer: {
+        paddingTop: 30,
+    },
+    welcomeContainer: {
+        alignItems: 'center',
+        marginTop: 10,
+        marginBottom: 20,
+    },
+    welcomeImage: {
+        width: 100,
+        height: 80,
+        resizeMode: 'contain',
+        marginTop: 3,
+        marginLeft: -10,
+    },
+    getStartedContainer: {
+        alignItems: 'center',
+        marginHorizontal: 50,
+    },
+    homeScreenFilename: {
+        marginVertical: 7,
+    },
+    codeHighlightText: {
+        color: 'rgba(96,100,109, 0.8)',
+    },
+    codeHighlightContainer: {
+        backgroundColor: 'rgba(0,0,0,0.05)',
+        borderRadius: 3,
+        paddingHorizontal: 4,
+    },
+    getStartedText: {
+        fontSize: 17,
+        color: 'rgba(96,100,109, 1)',
+        lineHeight: 24,
+        textAlign: 'center',
+    },
+    labelText: {
+        fontSize: 24,
+        textAlign: 'left',
+    },
+    tabBarInfoContainer: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        ...Platform.select({
+            ios: {
+                shadowColor: 'black',
+                shadowOffset: {height: -3},
+                shadowOpacity: 0.1,
+                shadowRadius: 3,
+            },
+            android: {
+                elevation: 20,
+            },
+        }),
+        alignItems: 'center',
+        backgroundColor: '#fbfbfb',
+        paddingVertical: 20,
+    },
+    tabBarInfoText: {
+        fontSize: 17,
+        color: 'rgba(96,100,109, 1)',
+        textAlign: 'center',
+    },
+    navigationFilename: {
+        marginTop: 5,
+    },
+    helpContainer: {
+        marginTop: 15,
+        alignItems: 'center',
+    },
+    helpLink: {
+        paddingVertical: 15,
+    },
+    helpLinkText: {
+        fontSize: 14,
+        color: '#2e78b7',
+    },
 });
+
+export default HomeScreen;
